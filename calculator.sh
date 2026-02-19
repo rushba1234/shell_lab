4f13034b89e2284dba0510cda6ab19c1083127b3
rushba1234 <rushbanunu@gmail.com>
2026-02-19 15:14:53 +0530

    done

---
+++ calculator.sh
@@ -0,0 +1,15 @@
+echo "enter x:"
+read X
+
+echo "enter y:"
+read Y
+
+echo "sum : $(expr $X + $Y)"
+echo "def : $(expr $X - $Y)"
+echo "pro : $(expr $X \* $Y)"
+echo "quoti : $(expr $X / $Y)"
+echo "rem : $(expr $X % $Y)"
+
+
+
+
